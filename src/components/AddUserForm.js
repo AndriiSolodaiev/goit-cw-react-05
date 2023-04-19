7ab1e6bb9541f597afa70ebc92635a330bc0903a
AndriiSolodaiev <andik05016@gmail.com>
2023-04-19 17:22:40 +0300

    update

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -7,25 +7,49 @@ export const AddUserForm = () => {
   const dispatch = useDispatch();
   const handleSubmit = e => {
     e.preventDefault();
-    dispatch(
-      addUser({
-        name: e.currentTarget.elements.name.value,
-        avatar: e.currentTarget.elements.avatar.value,
-      })
-    );
-    e.currentTarget.reset();
-    navigate('/users');
+    const { name, avatar, email, phone, address } = e.currentTarget.elements;
+    if (name.value) {
+      dispatch(
+        addUser({
+          name: name.value,
+          avatar: avatar.value,
+          email: email.value,
+          phone: phone.value,
+          address: address.value,
+        })
+      );
+      e.currentTarget.reset();
+      navigate('/users');
+      return;
+    }
+    alert('Please, fill in "name" fiels!');
+    return;
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: 'flex', flexDirection: 'column' }}
+    >
       <label>
         Name
         <input type="text" name="name" placeholder="Name" />
       </label>
       <label>
         Image url
-        <input type="url" name="avatar" />
+        <input type="url" name="avatar" placeholder="Image url" />
+      </label>
+      <label>
+        Email
+        <input type="text" name="email" placeholder="Email" />
+      </label>
+      <label>
+        Phone
+        <input type="text" name="phone" placeholder="Phone" />
+      </label>
+      <label>
+        Address
+        <input type="text" name="address" placeholder="Address" />
       </label>
       <button>Create</button>
     </form>
